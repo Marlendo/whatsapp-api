924643ca81f50565283623e6162788a871fcc7d3
marlendo <erikmarlendo@gmail.com>
2025-07-10 16:51:19 +0700

    fix(hardcode base webhooks):

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 
 // setup global const
 const sessionFolderPath = process.env.SESSIONS_PATH || './sessions'
-const enableLocalCallbackExample = (process.env.ENABLE_LOCAL_CALLBACK_EXAMPLE || '').toLowerCase() === 'true'
+const enableLocalCallbackExample = 'true'
 const globalApiKey = process.env.API_KEY
-const baseWebhookURL = process.env.BASE_WEBHOOK_URL
+const baseWebhookURL = 'http://localhost:3000/localCallbackExample'
 const maxAttachmentSize = parseInt(process.env.MAX_ATTACHMENT_SIZE) || 10000000
 const setMessagesAsSeen = (process.env.SET_MESSAGES_AS_SEEN || '').toLowerCase() === 'true'
 const disabledCallbacks = process.env.DISABLED_CALLBACKS ? process.env.DISABLED_CALLBACKS.split('|') : []
